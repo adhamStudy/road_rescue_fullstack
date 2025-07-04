a5a8ae1765acf742d46adcecaf700d3a6cb64cbf
adham <adhm000111000111@gmail.com>
2025-07-05 00:33:59 +0300

    inital commit start of project

---
+++ resources/js/Components/Hero.vue
@@ -0,0 +1,14 @@
+<template>
+
+    <div>
+        <h2 class="text-2xl">This is Hero Section</h2>
+    </div>
+
+</template>
+
+
+
+<script setup>
+
+
+</script>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="bg-blue-100 p-2 mb-4 flex justify-between">
+  <div>
+    <div class="bg-blue-100 p-2 mb-4 flex justify-between">
     <h1 class="w-fit font-semibold italic">My Company name</h1>
     <div>
       <ul class="flex space-x-3 mr-10 text-sm">
@@ -9,10 +10,19 @@
         <li class="hover:text-bold transition hover:text-blue-500 hover:cursor-pointer">
           About
         </li>
-        <li class="hover:text-bold transition hover:text-blue-500 hover:cursor-pointer">
-          <a href="/admin">Admin Panel</a>
-        </li>
+        
       </ul>
     </div>
   </div>
+  <Hero/>
+  </div>
+
+  <!-- Hero Section -->
+
 </template>
+
+<script setup>
+
+import Hero from '../Components/Hero.vue';
+
+</script>
