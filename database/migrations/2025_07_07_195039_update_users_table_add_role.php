cd0992d1ee87907f7573dc80c8872ffcff570448
adham <adhm000111000111@gmail.com>
2025-07-13 20:03:58 +0300

    remove role from users

--- database/migrations/2025_07_07_195039_update_users_table_add_role.php
+++ database/migrations/2025_07_07_195039_update_users_table_add_role.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->enum('role', ['visitor', 'admin'])->default('visitor')->after('password');
+            // $table->enum('role', ['visitor', 'admin'])->default('visitor')->after('password');
             //
         });
     }
