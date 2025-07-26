0046d6a372152b0381760b470b87d16b99a3ddfe
adham <adhm000111000111@gmail.com>
2025-07-26 23:26:14 +0300

    rmove nullable in phone

--- database/migrations/2025_07_16_190449_update_users_table_add_phone_number_to_users.php
+++ database/migrations/2025_07_16_190449_update_users_table_add_phone_number_to_users.php
@@ -10,7 +10,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('phone')->nullable();
+            $table->string('phone');
         });
     }
 
